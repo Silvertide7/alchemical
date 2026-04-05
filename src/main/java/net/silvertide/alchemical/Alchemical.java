772d716d4dae2fbbf560d1eb195ffd568d7e387c
Silvertide7 <natephillips801@gmail.com>
2026-04-04 21:18:24 -0600

    add initial item implementation

--- src/main/java/net/silvertide/alchemical/Alchemical.java
+++ src/main/java/net/silvertide/alchemical/Alchemical.java
@@ -1,24 +1,27 @@
 package net.silvertide.alchemical;
 
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.silvertide.alchemical.registry.DataComponentRegistry;
+import net.silvertide.alchemical.registry.ItemRegistry;
+import net.silvertide.alchemical.registry.TabRegistry;
 import org.slf4j.Logger;
 
-import com.mojang.logging.LogUtils;
-
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.ModContainer;
-
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Alchemical.MODID)
 public class Alchemical {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "alchemical";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Alchemical(IEventBus modEventBus, ModContainer modContainer) {
+        ItemRegistry.register(modEventBus);
+        DataComponentRegistry.register(modEventBus);
+        TabRegistry.register(modEventBus);
+    }
 
+    public static ResourceLocation id(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
     }
 }
